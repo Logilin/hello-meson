5d06fba8f6f47fbb5042497d76eedf71166dfc90
Christophe Blaess <christophe.blaess@logilin.fr>
2026-02-27 15:25:02 +0100

    Initial version.

---
+++ hello-meson.c
@@ -0,0 +1,21 @@
+#define _XOPEN_SOURCE  500
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/param.h>
+
+#ifndef MAXHOSTNAMELEN
+#define MAXHOSTNAMELEN  64
+#endif
+
+int main(void)
+{
+	char hostname[MAXHOSTNAMELEN];
+
+	gethostname(hostname, MAXHOSTNAMELEN);
+	hostname[MAXHOSTNAMELEN - 1] = '\0';
+
+	fprintf(stdout, "Hello from %s (built with meson)\n", hostname);
+
+	return EXIT_SUCCESS;
+}
